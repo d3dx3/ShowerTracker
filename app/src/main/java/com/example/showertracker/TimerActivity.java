084366545401d3ed483868a5df565ec60d767bd8
Larry Jensen <larrywymanj@gmail.com>
2019-05-02 15:36:42 -0700

    gallons calculated

--- app/src/main/java/com/example/showertracker/TimerActivity.java
+++ app/src/main/java/com/example/showertracker/TimerActivity.java
@@ -1,10 +1,13 @@
 package com.example.showertracker;
 
+import android.content.res.Resources;
 import android.os.SystemClock;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Chronometer;
+import android.widget.TextView;
 
 import com.jjoe64.graphview.series.BarGraphSeries;
 import com.jjoe64.graphview.series.DataPoint;
@@ -36,6 +39,12 @@ public class TimerActivity extends AppCompatActivity {
 
         Date currentTime = Calendar.getInstance().getTime();
         long time = (SystemClock.elapsedRealtime() - cTimer.getBase()) / 1000;
+        double gallonsused = time * 0.042;
+        Log.d ("gallons", String.valueOf(gallonsused));
+        TextView timetook = findViewById(R.id.timetook);
+        Resources res = getResources();
+        String text = String.format(res.getString(R.string.timetookplacement), gallonsused);
+        timetook.setText(text);
 
         BarGraphSeries<DataPoint> newSeries = new BarGraphSeries<> (new DataPoint[]
                 {
