92fb4ecdd78ad69ca6b7c3aca04f62dcde67fe58
CormacGG <glee8412@pacificu.edu>
2019-05-03 11:42:28 -0700

    gorboge

--- app/src/main/java/com/example/showertracker/GlobalApplication.java
+++ app/src/main/java/com/example/showertracker/GlobalApplication.java
@@ -4,6 +4,16 @@ import android.app.Application;
 
 import com.jjoe64.graphview.series.BarGraphSeries;
 import com.jjoe64.graphview.series.DataPoint;
+import java.io.*;
+
+
+import java.io.BufferedWriter;
+import java.util.Scanner;
+import java.io.*;
+import android.os.Environment;
+import java.util.Date;
+import java.text.SimpleDateFormat;
+
 
 import java.util.Date;
 
@@ -15,7 +25,31 @@ public class GlobalApplication extends Application {
         return series;
     }
     public void setSeries (BarGraphSeries<DataPoint> newSeries) {
-        this.series = newSeries;
+
+        try {
+            File file;
+            file = new File(Environment.getExternalStorageDirectory() + File.separator + "database.txt");
+
+            Scanner scan = new Scanner(file);
+
+            Date tempDate;
+            String str;
+            int last;
+
+            while (scan.hasNextLine()) {
+
+                str = (String)scan.nextLine();
+                last = str.lastIndexOf(' ');
+                String dateStr = str.substring(0, last);
+
+                Date date1 = new SimpleDateFormat("E, MMM dd yyyy HH:mm:ss").parse(dateStr);
+            }
+
+            this.series = newSeries;
+        }
+        catch (Exception e) {
+            System.out.println ("failed");
+        }
     }
 
     public void setgDateTime (Date newtime) {
--- app/src/main/java/com/example/showertracker/TimerActivity.java
+++ app/src/main/java/com/example/showertracker/TimerActivity.java
@@ -9,12 +9,23 @@ import android.view.View;
 import android.widget.Chronometer;
 import android.widget.TextView;
 
+import java.io.FileWriter;
+import java.io.PrintWriter;
+
 import com.jjoe64.graphview.series.BarGraphSeries;
 import com.jjoe64.graphview.series.DataPoint;
 import com.jjoe64.graphview.series.LineGraphSeries;
 
 import java.util.Calendar;
 import java.util.Date;
+
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+import java.io.BufferedWriter;
+import java.util.Scanner;
+import java.io.*;
+import android.os.Environment;
 
 public class TimerActivity extends AppCompatActivity {
 
@@ -37,8 +48,41 @@ public class TimerActivity extends AppCompatActivity {
         //TODO save to DB
         //TODO fix times
 
+//        PrintWriter file = new PrintWriter ("database.txt", "UTF-8");
+
         Date currentTime = Calendar.getInstance().getTime();
         long time = (SystemClock.elapsedRealtime() - cTimer.getBase()) / 1000;
+
+        String tester = "Test";
+        File file;
+        FileWriter fwriter;
+        BufferedWriter writer;
+        try {
+            file = new File (Environment.getExternalStorageDirectory() + File.separator + "database.txt");
+            if (file.exists())
+            {
+                file.delete();
+            }
+            file.createNewFile();
+            fwriter = new FileWriter (file);
+            writer = new BufferedWriter(fwriter);
+            String test = "Test Insert";
+
+            writer.write(currentTime + " " + time + "\n");
+            writer.close();
+            Scanner scan = new Scanner(file);
+
+            if (file.length() == 0)
+            {
+                return;
+            }
+
+
+
+        } catch (Exception e) {
+            System.out.println("Failed");
+        }
+
         double gallonsused = time * 0.042;
         Log.d ("gallons", String.valueOf(gallonsused));
         TextView timetook = findViewById(R.id.timetook);
@@ -52,6 +96,8 @@ public class TimerActivity extends AppCompatActivity {
                 });
         ((GlobalApplication)this.getApplication()).setSeries(newSeries);
         cTimer.setBase(SystemClock.elapsedRealtime());
+
+
     }
 
 
